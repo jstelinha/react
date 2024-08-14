c0566c33e5b0cee508bd9729e9737d7d62f1216a
stelinha <oompaloompas007@gmail.com>
2024-08-14 11:57:55 -0300

    Header
    Header

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,30 +1,18 @@
-
+import {Header, teste} from "./components/Header";
 
 function App() {
-  let nome = "Stela";
 
-const soma = (a, b) => {
-  return a + b
-}
+  return(
+    <div> 
+      {/* Header */}
+      <Header> </Header>
+      {/*  Main  */}
 
-const saudacao = (nome) => {
-  return  <h2> seja bem vindo {nome}!</h2>
-}
+      {/* Footer */}
 
-  return (
-    <>
-      <h1> olá </h1>
-    
-      <p> Lorem ipsum dolor sit amet. Qui alias placeat qui magni rerum qui praesentium unde. Sed quasi quaerat et dolore consectetur et nesciunt nihil. Et aspernatur alias id commodi obcaecati 33 mollitia voluptates eos quia officiis non dolorum consectetur.
-      </p>
+    </div>
 
-      <p>
-        2 + 2 = 
-        { 2+2 }
-      </p>
-    </>
-  );
-
+  )
 }
 
 export default App;
---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,22 @@
+// import "./Header.css"
+import styles from "./Header.css"
+
+let nome = "Joao"
+
+export function Header(){
+
+    return(
+
+
+        
+        <div className={styles.container}>
+            <h1> Header </h1>
+            <h2> aaaa </h2>
+        </div>
+    )
+}
+
+// export default Header 
+export function teste(){
+    return "sou um teste"
+}
